c3ca2df62433ee1234fdc0efe63d2577daa8b6cf
Nasa-ee <nasaee.dev@gmail.com>
2023-03-06 02:54:06 +0700

    finish final ep.68

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from '../images/logo.svg';
-import { pageLinks, socialLinks } from '../data';
+import { socialLinks } from '../data';
+import PageLinks from './PageLinks';
+import SocialLink from './SocialLink';
 
 const Navbar = () => {
   return (
@@ -12,36 +14,13 @@ const Navbar = () => {
             <i className="fas fa-bars"></i>
           </button>
         </div>
-        {/* <!-- left this comment on purpose --> */}
-        <ul className="nav-links" id="nav-links">
-          {pageLinks.map((link) => {
-            const { id, href, text } = link;
-            return (
-              <li key={id}>
-                <a href={href} className="nav-link">
-                  {text}
-                </a>
-              </li>
-            );
-          })}
-        </ul>
+
+        <PageLinks parentClass="nav-links" itemClass="nav-link" />
 
         <ul className="nav-icons">
-          {socialLinks.map((link) => {
-            const { id, href, icon } = link;
-            return (
-              <li key={id}>
-                <a
-                  href={href}
-                  target="_blank"
-                  rel="noreferrer"
-                  className="nav-icon"
-                >
-                  <i className={icon}></i>
-                </a>
-              </li>
-            );
-          })}
+          {socialLinks.map((link) => (
+            <SocialLink key={link.id} {...link} itemClass="nav-icon" />
+          ))}
         </ul>
       </div>
     </nav>
